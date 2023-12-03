c84c63ac7bc1d671cf38162e86e61b961573d937
NAINSOO <68545724+NAINSOO@users.noreply.github.com>
2023-12-03 21:57:46 +0900

    Update views.py
    user 생성 기능 추가
    user 보기 기능 추가

--- apps/crud/views.py
+++ apps/crud/views.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, render_template
 from apps.app import db
 from apps.crud.models import User
+from apps.crud.forms import UserForm
+from flask import redirect, url_for, flash
+from sqlalchemy import exc
 
 crud = Blueprint("crud",
                  __name__,
@@ -20,3 +23,30 @@ def sql():
     db.session.commit()
 
     return "콘솔 로그를 확인해 주세요"
+
+@crud.route("/users/new", methods=["GET","POST"])
+def create_user():
+    form = UserForm()
+    if form.validate_on_submit():
+        user = User(
+            username = form.username.data,
+            email = form.email.data,
+            password = form.password.data,              
+        )
+        try:
+            db.session.add(user)
+            db.session.commit()
+        except exc.IntegrityError as error:
+            if error.orig.args[0] == 1062:
+                flash("중복된 이메일 주소입니다.")
+                return redirect(url_for("crud.create_user"))
+            else:
+                flash('MySQL error: {}'.format(error))
+                return redirect(url_for("crud.create_user"))
+        return redirect(url_for("crud.users"))
+    return render_template("crud/create.html", form=form)
+
+@crud.route("/users")
+def users():
+    users = User.query.all()
+    return render_template("crud/index.html",users=users)
