6860dc0c83a613f23c5e64b995ece910a3af1118
NAINSOO <68545724+NAINSOO@users.noreply.github.com>
2023-12-03 21:57:01 +0900

    Update app.py
    csrf 기능 추가

--- apps/app.py
+++ apps/app.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_wtf.csrf import CSRFProtect
 
 db = SQLAlchemy()
+csrf = CSRFProtect()
 
 def create_app():
     app = Flask(__name__)
+
+    app.config['WTF_CSRF_SECRET_KEY']="randomvalue1234000"
+
+    csrf.init_app(app)
 
     app.config['SECRET_KEY'] ="1234"
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@localhost:3306/testdb'
