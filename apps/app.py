06e6c446b445e1c90f2ce0bd080e27c6f3f54e2f
NAINSOO <68545724+NAINSOO@users.noreply.github.com>
2023-12-05 03:04:53 +0900

    Update app.py
    config.py  를 통해서 환경 설정

--- apps/app.py
+++ apps/app.py
@@ -2,26 +2,29 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
+from apps.config import config
 
 db = SQLAlchemy()
 csrf = CSRFProtect()
 
-def create_app():
+def create_app(config_key):
     app = Flask(__name__)
 
-    app.config['WTF_CSRF_SECRET_KEY']="randomvalue1234000"
+    app.config.from_object(config[config_key])
 
     csrf.init_app(app)
 
-    app.config['SECRET_KEY'] ="1234"
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@localhost:3306/testdb'
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["SQLALCHEMY_ECHO"] = True
     db.init_app(app)
 
     Migrate(app, db)
 
     from apps.crud import views as crud_views
-
     app.register_blueprint(crud_views.crud, url_prefix="/crud")
+
+    from apps.auth import views as auth_views
+    app.register_blueprint(auth_views.auth, url_prefix="/auth")
+
+    from apps.board import views as board_views
+    app.register_blueprint(board_views.board, url_prefix="/board")
+
     return app
