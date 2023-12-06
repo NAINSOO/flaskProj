aa9fe4f80a04d11c4ce52fa279cbc0f6dbb80996
NAINSOO <68545724+NAINSOO@users.noreply.github.com>
2023-12-06 17:50:42 +0900

    Update views.py
    edit_board : 글 수정 라우터 함수 생성
    delete_board : 글 삭제 라우터 함수 생성

--- apps/board/views.py
+++ apps/board/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, session, g, flash
+from flask import Blueprint, render_template, redirect, url_for, session, g, flash, request
 from apps.board.models import Board
 from apps.board.forms import BoardForm
 from apps.app import db
@@ -43,3 +43,57 @@ def detail_board(board_id):
     return render_template("board/detail.html",board = board)
 
 
+@board.route("/edit/<board_id>", methods=['get', 'post'])
+def edit_board(board_id):
+    board = Board.query.filter_by(id=board_id).first()
+    user = g.user
+
+    if user == None:
+        flash("로그인 상태가 아닙니다.")
+        return redirect(url_for('auth.login'))
+    elif board == None:
+        flash("글이 존재하지 않습니다.")
+        return redirect(url_for('board.index'))
+    elif user.id != board.user_id:
+        flash("글쓴이가 아닙니다.")
+        return redirect(url_for('board.index'))
+    
+    form = BoardForm()
+    if request.method == 'GET': 
+        form.title.data = board.title
+        form.content.data = board.content
+    if form.validate_on_submit():
+        board.title = form.title.data
+        board.content = form.content.data
+        try:
+            db.session.add(board)
+            db.session.commit()
+        except exc.IntegrityError as error:
+            flash('MySQL error: {}'.format(error))
+            return redirect(url_for("board.edit_board"))
+        return redirect(url_for('board.index'))
+
+    return render_template('board/edit.html',form=form, board=board)
+
+@board.route("delete/<board_id>", methods=['get'])
+def delete_board(board_id):
+    board = Board.query.filter_by(id=board_id).first()
+    user = g.user
+
+    if user == None:
+        flash("로그인 상태가 아닙니다.")
+        return redirect(url_for('auth.login'))
+    elif board == None:
+        flash("글이 존재하지 않습니다.")
+        return redirect(url_for('board.index'))
+    elif user.id != board.user_id:
+        flash("글쓴이가 아닙니다.")
+        return redirect(url_for('board.index'))
+    try:
+        db.session.delete(board)
+        db.session.commit()
+    except exc.IntegrityError as error:
+        flash('MySQL error: {}'.format(error))
+        return redirect(url_for("board.edit_board"))
+
+    return redirect(url_for('board.index'))
