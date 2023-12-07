ea00e0f4648628e8ad3ed74264b8480e18b714d0
NAINSOO <68545724+NAINSOO@users.noreply.github.com>
2023-12-07 21:00:49 +0900

    Update views.py
    create_answer 과 delete_anwer 라우터 함수 추가

--- apps/board/views.py
+++ apps/board/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, session, g, flash, request
-from apps.board.models import Board
-from apps.board.forms import BoardForm
+from apps.board.models import Board, Answer
+from apps.board.forms import BoardForm, AnswerForm
 from apps.app import db
 import datetime
 from sqlalchemy import exc
@@ -40,7 +40,9 @@ def create_board():
 @board.route("/detail/<board_id>", methods=["get", "post"])
 def detail_board(board_id):
     board = Board.query.filter_by(id=board_id).first()
-    return render_template("board/detail.html",board = board)
+    form = AnswerForm()
+
+    return render_template("board/detail.html",board = board, form=form)
 
 
 @board.route("/edit/<board_id>", methods=['get', 'post'])
@@ -97,3 +99,48 @@ def delete_board(board_id):
         return redirect(url_for("board.edit_board"))
 
     return redirect(url_for('board.index'))
+
+@board.route("/detail/<board_id>/answer", methods=['post'])
+def create_answer(board_id):
+    user = g.user
+    board = Board.query.filter_by(id=board_id).first()
+
+    if user == None:
+        flash("로그인 상태가 아닙니다.")
+        return redirect(url_for('auth.login'))
+    elif board == None:
+        flash("글이 존재하지 않습니다.")
+        return redirect(url_for('board.index'))
+    form = AnswerForm()
+    if form.validate_on_submit():
+        answer = Answer(content = form.content.data, user_id = user.id, board_id = board.id ,create_date = datetime.datetime.now())
+        try:
+            db.session.add(answer)
+            db.session.commit()
+        except exc.IntegrityError as error:
+            flash('MySQL error: {}'.format(error))
+            return redirect(url_for("board.detail_board",board_id=board.id))
+    return redirect(url_for('board.detail_board',board_id=board.id))
+
+@board.route("/detail/<board_id>/answer/<answer_id>/delete", methods=['get'])
+def delete_answer(board_id, answer_id):
+    board = Board.query.filter_by(id=board_id).first()
+    user = g.user
+    answer = Answer.query.filter_by(id=answer_id, user=user, board=board).first()
+    if user == None:
+        flash("로그인 상태가 아닙니다.")
+        return redirect(url_for('auth.login'))
+    elif board == None:
+        flash("글이 존재하지 않습니다.")
+        return redirect(url_for('board.index'))
+    elif user.id != answer.user_id:
+        flash("글쓴이가 아닙니다.")
+        return redirect(url_for('board.index'))
+    try:
+        db.session.delete(answer)
+        db.session.commit()
+    except exc.IntegrityError as error:
+        flash('MySQL error: {}'.format(error))
+        redirect(url_for('board.detail_board',board_id=board.id))
+
+    return redirect(url_for('board.detail_board',board_id=board.id))
